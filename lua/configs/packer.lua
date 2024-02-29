1591178ed45d0538bf32ebc49a621a918031178f
gplubeck <78976355+gplubeck@users.noreply.github.com>
2024-02-29 11:43:42 -0500

    Update packer.lua

--- lua/configs/packer.lua
+++ lua/configs/packer.lua
@@ -22,6 +22,20 @@ vim.cmd('colorscheme rose-pine')
 -- install tree sitter
 use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
+-- WhichKey plugin
+use {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section
+    }
+  end
+}
+
 -- LSP Setup
   use {
           'VonHeikemen/lsp-zero.nvim',
